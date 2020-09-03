c6777ccb1ca9347fb93c5f495d35e16de374e8b0
lizihao <1318844065@qq.com>
2020-09-03 09:55:37 +0800

    全院跟新1

--- src/main/java/com/follow/vo/DepartmentHospitalVo.java
+++ src/main/java/com/follow/vo/DepartmentHospitalVo.java
@@ -4,15 +4,15 @@ import lombok.Data;
 
 @Data
 public class DepartmentHospitalVo {
-
+    //科室id
     private Integer id ;
-
+    //科室名称
     private String departmentName;
-
+    //医院名称
     private String hospitalName;
-
+    //医院id
     private Integer hospitalId ;
-
+    //科室负责人
     private String departmentBoss ;
 
 }
